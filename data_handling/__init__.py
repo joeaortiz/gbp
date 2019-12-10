7e20f97d3b8963717ea2ccb17c518f5a15c80e42
joe <joe.ortiz@hotmail.co.uk>
2019-12-10 19:16:43 +0000

    moved necessary data handling file into utils

--- data_handling/__init__.py
+++ data_handling/__init__.py
@@ -1,3 +1,2 @@
 from . import gen_data
 from . import parse_orbslam_res
-from . import read_balfile
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from . import lie_algebra
 from . import transformations
+from . import read_balfile
 
